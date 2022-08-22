839f17dbcadeba34b9a1d7d020a96f7eb8e1545c
SeanDevanny <s_devanny@modular-global.com>
2022-08-22 16:56:40 +0100

    Finally got the input area working. There was a bug with the file reader.

--- js/TableBuilder.js
+++ js/TableBuilder.js
@@ -1,19 +1,45 @@
 //XMLHttpRequest() - has responseXML property in the response
-        
-document.addEventListener('DOMContentLoaded', ()=>{
-    //fetch the data as soon as the page has loaded
-    let url = "ST25918PLC01R0400_LaserWelding.L5X";
-    fetch(url)
-    .then(response=>response.text())
-    .then(data=>{
-        //console.log(data);  //string
-        let parser = new DOMParser();
-        let xml = parser.parseFromString(data, "application/xml");
-        //document.getElementById('output').textContent = data;//writes xml data to the 'output' html pre element
-        //console.log(xml);
-        buildDataLoggerResultset(xml);
-    });
-})
+
+function getXMLFiles(event){
+    //this.preventDefault();
+    if (event === undefined) {
+        console.log(inVar.not)
+    }
+
+    var file = document.getElementById('FileInput');
+
+    if (file.files.length) {
+        var reader = new FileReader();
+    
+        reader.onload = function(e) {
+        /* let data = e.target.result; */
+        //   document.getElementById('outputDiv').innerHTML = e.target.result;
+          console.log(e.target.result);//why am I slicing 3 charaters from the string? No idea. The file reader for some reason adds the 3 characters ï»¿ to the front of an .L5X XML file because it's almost as dumb as me. This took too long to find. I hate it here. 
+          let parser = new DOMParser();
+          let xml = parser.parseFromString(e.target.result.slice(3), "application/xml");
+          console.log(xml);
+          buildDataLoggerResultset(xml);
+        };
+    
+        reader.readAsBinaryString(file.files[0]);
+      }
+};
+
+// document.addEventListener('DOMContentLoaded', ()=>{
+//     //fetch the data as soon as the page has loaded
+//         let url = "ST25918PLC01R0400_LaserWelding.L5X";
+//         console.log(url);
+//         fetch(url)
+//         .then(response=>response.text())
+//         .then(data=>{
+//             //console.log(data);  //string
+//             let parser = new DOMParser();
+//             let xml = parser.parseFromString(data, "application/xml");
+//             //document.getElementById('output').textContent = data;//writes xml data to the 'output' html pre element
+//             console.log(xml);
+//             buildDataLoggerResultset(xml);
+//     });
+// })
 
 //textContents is all text contained by an element and all its children that are for formatting purposes only. innerText returns all text contained by an element and all its child elements. innerHtml returns all text, including html tags, that is contained by an element.
 
